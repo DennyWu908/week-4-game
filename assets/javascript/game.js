64844064d23b694cbec4b9bdbdfbf1dc977e2b4e
dblanchard13 <davidb.prof@gmail.com>
2017-09-16 01:14:32 -0700

    Homework comments.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,52 @@
+// Nice job putting everything insude the document ready block 👌
 $(document).ready(function () {
 
-// First, I need to create a variable for the target number. This is the number that someone playing the game must meet in order to win. It is randomly generated number and will be between 19 and 120. I need to check the code for the generation of this number.
+// A big chunk of this logic is repeated a few times throughout
+// your app. It would be much more DRY to put it all in a function
+// and just call that everytime you need to reset the crystal values
+// and goalNumber
 
-var goalNumber = Math.floor((Math.random() * 102) + 19);
+// Define all your game variables outside the function so they can be accessed
+// anywhere within this document ready scope
+var goalNumber, crystalOne, crystalTwo, crystalThree, crystalFour, playerScore
 
-// This line will print the value of the target number to the correct div.
+function initializeCrystalsAndGoalNumber() {
+	// First, I need to create a variable for the target number. This is the number that someone playing the game must meet in order to win. It is randomly generated number and will be between 19 and 120. I need to check the code for the generation of this number.
 
-$(".target-number").html(goalNumber);
+	// Since the string of code for generating random numbers
+	// is rather unwieldy I'd suggest placing it in a utility function
+	goalNumber = getRandomNum(120, 19)
+
+	// This line will print the value of the target number to the correct div.
+
+	$(".target-number").html(goalNumber);
+
+
+	// Next, I need variables for the crystals. The value of each variable is also a randomly generated number (between 1 and 12), which the player must add to their score. The goal of the game is to have a score equal to the target number.
+
+	crystalOne = getRandomNum(12, 1)
+
+	crystalTwo = getRandomNum(12, 1)
+
+	crystalThree = getRandomNum(12, 1)
+
+	crystalFour = getRandomNum(12, 1)
+
+	// When the game starts, the player's score will be 0.
+
+	playerScore = 0
+
+	$(".player-score").html(playerScore);
+
+}
+
+initializeCrystalsAndGoalNumber()
+
+// Utility function for generating a random number
+function getRandomNum (max, min) {
+	return Math.floor(Math.random() * (max - min) + min)
+}
+
 
 // The following are variables for the player's number of wins and losses.
 
@@ -17,22 +57,6 @@ $("#player-wins").html("Wins: " + playerWins)
 var playerLosses = 0
 
 $("#player-losses").html("Losses: " + playerLosses)
-
-// Next, I need variables for the crystals. The value of each variable is also a randomly generated number (between 1 and 12), which the player must add to their score. The goal of the game is to have a score equal to the target number.
-
-var crystalOne = Math.floor((Math.random() * 12) + 1);
-
-var crystalTwo = Math.floor((Math.random() * 12) + 1);
-
-var crystalThree = Math.floor((Math.random() * 12) + 1);
-
-var crystalFour = Math.floor((Math.random() * 12) + 1);
-
-// When the game starts, the player's score will be 0.
-
-var playerScore = 0
-
-$(".player-score").html(playerScore);
 
 // When any of the crystal buttons is clicked, the value will be added to the the player's score.
 
@@ -67,34 +91,21 @@ $(".number-button").on("click", function() {
 		playerWins += 1
 		$("#player-wins").html("Wins: " + playerWins)
 
-		playerScore = 0
-		$(".player-score").html(playerScore);
+		initializeCrystalsAndGoalNumber()
 
-		crystalOne = Math.floor((Math.random() * 12) + 1);
-		crystalTwo = Math.floor((Math.random() * 12) + 1);
-		crystalThree = Math.floor((Math.random() * 12) + 1);
-		crystalFour = Math.floor((Math.random() * 12) + 1);
-
-		goalNumber = Math.round(((Math.random() + 1) * 25))
-		$(".target-number").html(goalNumber);
 		alert("You Win!")
 
 	} else if (playerScore > goalNumber) {
 
 		playerLosses += 1
 		$("#player-losses").html("Losses: " + playerLosses)
-		console.log("Losses: " + playerLosses)
 
-		playerScore = 0
-		$(".player-score").html(playerScore);
+		// It's generally considered best practice to remove console.logs
+		// from your code before pushing it to production.
+		// console.log("Losses: " + playerLosses)
 
-		crystalOne = Math.floor((Math.random() * 12) + 1);
-		crystalTwo = Math.floor((Math.random() * 12) + 1);
-		crystalThree = Math.floor((Math.random() * 12) + 1);
-		crystalFour = Math.floor((Math.random() * 12) + 1);
+		initializeCrystalsAndGoalNumber()
 
-		goalNumber = Math.round(((Math.random() + 1) * 25))
-		$(".target-number").html(goalNumber);
 		alert("Sorry, you lost.")
 
 	}
